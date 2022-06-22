8bc106debd196d3247e788cb81da8b5d82a38f85
Dilan_105248 <dilan.deshapriya@axiatadigitallabs.com>
2022-06-22 11:32:03 +0530

    data source changed for bank service

--- src/main/java/com/javademo/account/AccountApplication.java
+++ src/main/java/com/javademo/account/AccountApplication.java
@@ -1,13 +1,67 @@
 package com.javademo.account;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AccountApplication {
+import com.javademo.account.entity.Account;
+import com.javademo.account.entity.Address;
+import com.javademo.account.entity.Customer;
+import com.javademo.account.repository.AccountRepository;
+import com.javademo.account.repository.AddressRepository;
+import com.javademo.account.repository.CustomerRepository;
+import com.javademo.account.service.CustomerService;
 
+@SpringBootApplication
+public class AccountApplication implements CommandLineRunner{
+
+	@Autowired
+	CustomerRepository repo;
+	
+	@Autowired
+	AddressRepository adddressRepo;
+	
+	@Autowired
+	AccountRepository accountRepo;
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
+		
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		adddressRepo.save(new Address("street", "building", "address1", "address2", "city", "state"));
+		
+		
+		
+		Customer customer = new Customer();
+		customer.setActive(true);
+		customer.setName("Sai");
+		customer.setPanNo("123");
+		customer.setPrimaryAccountNo("123456");
+		customer.setEmailId("sai@gmail.com");
+		customer.setImei("12");
+		customer.setLatestLoggedIn(new Date());
+		
+		
+		
+		customer.setAddress(adddressRepo.findById(1));
+		
+		List<Account> accountInfoList = new ArrayList<>();
+		accountInfoList.add(new Account("12345", "3456", "test", "test", 100.00, customer));
+		
+		customer.setAccountInfo(accountInfoList);
+		repo.save(customer);
+		
 	}
 
 }
